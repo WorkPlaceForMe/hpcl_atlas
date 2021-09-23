ccab24206f19c8d9423180aed08028c5bbfb17ec
graymatics <sample@graymatics.com>
2021-09-23 15:45:58 +0800

    before edit

--- run_all.sh
+++ run_all.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-dir0="/home/hpcl-videoanalytics/atlas2/"
-src="${dir0}src/final4/"
-#dir0="/home/graymatics/dev/atlas/"
-#src="${dir0}final4/"
-
-dock="${dir0}docker/"
-yolo="${dir0}src/General_Yolov3_upgrade/bin/"
-
-MYSQL_DB=hpcl3
-
-cd $dock
-bash run-docker-ff.sh
-sleep 5
-
-cd $src
-echo Huawei12#$ | sudo -S pkill -f "run_objectdemo.py"
-#echo sudo -S python3 -u run_objectdemo.py --src $src --yolo $yolo --db $MYSQL_DB &
-sudo python3 -u run_objectdemo.py --src $src --yolo $yolo --db $MYSQL_DB &
-sleep 5
+#dir0="/home/hpcl-videoanalytics/atlas2/"
+#src="${dir0}src/final4/"
+##dir0="/home/graymatics/dev/atlas/"
+##src="${dir0}final4/"
+#
+#dock="${dir0}docker/"
+#yolo="${dir0}src/General_Yolov3_upgrade/bin/"
+#
+#MYSQL_DB=hpcl3
+#
+#cd $dock
+#bash run-docker-ff.sh
+#sleep 5
+#
+#cd $src
+#echo Huawei12#$ | sudo -S pkill -f "run_objectdemo.py"
+##echo sudo -S python3 -u run_objectdemo.py --src $src --yolo $yolo --db $MYSQL_DB &
+#sudo python3 -u run_objectdemo.py --src $src --yolo $yolo --db $MYSQL_DB &
+#sleep 5
+#    
+#cd $dock
+#bash run-docker.sh $src $MYSQL_DB
+#
+#while true;do
+#    sleep 99999
+#done
     
-cd $dock
-bash run-docker.sh $src $MYSQL_DB
-
-while true;do
-    sleep 99999
-done
-    
-
-
-
+cd /home/hpcl-videoanalytics/atlas2/src/final4
+bash run_all_server.sh > run_server.log &
+bash run_all_client.sh
